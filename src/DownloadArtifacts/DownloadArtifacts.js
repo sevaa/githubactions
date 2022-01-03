51cec7fc95cca02cf635760e8b1d08649d26a5d5
Seva Alekseyev <sevaa@nih.gov>
2022-01-03 13:46:39 -0500

    1.20, with artifact download

---
+++ src/DownloadArtifacts/DownloadArtifacts.js
@@ -0,0 +1,125 @@
+const tl = require("azure-pipelines-task-lib/task");
+const rimraf = require('rimraf');
+const fs = require('fs');
+const axios = require("axios").default;
+const nodeStreamZip = require("node-stream-zip");
+
+let axConf, repo;
+
+async function ghGet(url)
+{
+    return axios.get(`https://api.github.com/repos/${repo}${url}`, axConf);
+}
+
+async function main()
+{
+    try
+    {   
+        let ghToken, workflowName;
+        if(tl.getVariable("Agent.Version")) //Running from the agent
+        {
+            const auth = tl.getEndpointAuthorization(tl.getInput("gh"), false).parameters;
+            if("accessToken" in auth && auth.accessToken)
+                ghToken = auth.accessToken;
+            else if("AccessToken" in auth && auth.AccessToken)
+                ghToken = auth.AccessToken;
+            else
+                throw new Error("Unable to retrieve the GitHub token from the service endpoint.")
+
+            repo = tl.getInput("repo");
+            workflowName = tl.getInput("workflow");
+        }
+        else //Interactive run
+        {
+            ghToken = process.env.GITHUB_TOKEN;
+            repo = process.argv[2];
+            workflowName = process.argv[3];
+        }
+
+        axConf = {headers:
+            {
+                "Authorization": `token ${ghToken}`,
+                "Accept": "application/vnd.github.v3+json",
+                "User-Agent": "Azure DevOps task sevaalekseyev/GithubActions/DownloadArtifacts"
+            }};
+
+        //Find the right workflow
+        const workflows = (await ghGet(`/actions/workflows`, axConf)).data.workflows;
+        let workflow;
+        if(workflows.length == 0)
+            throw new Error("There are no workflows in the specified repository.");
+        if(workflowName)
+        {
+            workflow = workflows.find(wf => wf.name.toLowerCase() == workflowName.toLowerCase());
+            if(!workflow)
+                throw new Error(`There is no workflow named ${workflowName} in the repository.`);
+        }
+        else
+            workflow = workflows[0];
+        console.log(`Found the workflow, #${workflow.id}/${workflow.name}.`);
+
+        // Retrieve the runs, get the last one
+        const runs = (await ghGet("/actions/runs")).data.workflow_runs
+            .filter(r => r.workflow_id == workflow.id && r.status == 'completed' && r.conclusion == 'success')
+            .sort((l,r) => r.run_number - l.run_number);
+        if(runs.length == 0)
+        {
+            tl.error(`The workflow ${workflow.name} has no successful runs.`);
+            process.exit(1);
+        }
+        const run = runs[0], runID = run.id;
+        console.log(`Found the last successful run, started at #${run.run_started_at}.`);
+
+        //Download the artifacts, if any
+        const artifacts = (await ghGet(`/actions/runs/${runID}/artifacts`)).data.artifacts;
+        //Axios config for streaming archive downloads
+        const conf = {responseType: "stream", headers: axConf.headers};
+        console.log(`Downloading artifacts under ${process.cwd()}.`)
+
+        for(const artifact of artifacts)
+        {
+            const artName = artifact.name;
+            try
+            {
+                console.log(`Downloading artifact ${artName}...`);
+                const zipDownload = await axios.get(artifact.archive_download_url, conf);
+                const zipFileName = `${artName}.zip`;
+                const fst = fs.createWriteStream(zipFileName);
+                zipDownload.data.pipe(fst);
+                //Not sure about this line. Will error on read end close the write end?
+                await new Promise((resolve, err) => {fst.on("finish", resolve); fst.on("error", err); zipDownload.data.on("error", err)});
+
+                const st = fs.statSync(zipFileName);
+                console.log(`Downloaded artifact ${artName} - ${st.size} bytes, unzipping...`);
+
+                //Unzip it. Ideally, unzip straight from the web response stream would be cute.
+                if(fs.existsSync(artName))
+                {
+                    if(fs.statSync(artName).isDirectory())
+                        rimraf.sync(artName);
+                    else
+                        fs.unlinkSync(artName);
+                }
+                fs.mkdirSync(artName);
+                const zipFile = new nodeStreamZip.async({file: zipFileName});
+                await zipFile.extract(null, artName);
+                fs.unlinkSync(zipFileName);
+            }
+            catch(exc)
+            {
+                tl.warning(`Error while downloading artifact ${artName}: ${exc.stack}`);
+                if(!!exc.isAxiosError)
+                    tl.warning(exc.response.data.message);
+            }                
+        }
+    }
+    catch(exc)
+    {
+        tl.error(exc.stack);
+        if(!!exc.isAxiosError)
+            tl.error(exc.response.data.message);
+        process.exit(1);
+    }
+}
+
+main();
--- src/readme.md
+++ src/readme.md
@@ -4,6 +4,8 @@ This extension provides a build/release task for Azure DevOps pipelines
  that starts a GitHub workflow (an action), waits until the action is complete,
 and downloads the published artifacts, if any.
 
+Another task only downloads the artifacts from the last successful run.
+
 Only https://github.com/ is supported. Enterprise GitHub is not supported.
 
-The action must be set up with a `workflow_dispatch` trigger in the workflow's YAML file on GitHub. Extra inputs are supported, as long as they are declared in the YAML under `on/workflow_dispatch/inputs`.
+For the "Run Workflow" task, the action must be set up with a `workflow_dispatch` trigger in the workflow's YAML file on GitHub. Extra inputs are supported, as long as they are declared in the YAML under `on/workflow_dispatch/inputs`.
